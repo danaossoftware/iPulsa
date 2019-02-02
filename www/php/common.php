d2e58fc6597ff41b6762cb348babbeb66ecd5c8a
Dana Prakoso <danaossoftware@gmail.com>
2019-02-02 16:38:02 +0700

    commit

--- www/php/common.php
+++ www/php/common.php
@@ -4,8 +4,8 @@ include 'get-ip.php';
 function getUserID() {
     $c = new mysqli("localhost", "u954666570_pulsa", "HelloWorld123");
     $c->select_db("u954666570_pulsa");
-    /*$ip = getIP();
-    $results = $c->query("SELECT * FROM sessions WHERE ip='" . $ip . "'");
+    $ip = getIP();
+    /*$results = $c->query("SELECT * FROM sessions WHERE ip='" . $ip . "'");
     if ($results && $results->num_rows > 0) {
         return $results["user_id"];
     } else {

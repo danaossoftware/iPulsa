f3386caa494191dcc57f323eac312245e8f03edb
Dana Prakoso <danaossoftware@gmail.com>
2019-02-02 15:45:03 +0700

    commit

--- www/php/login.php
+++ www/php/login.php
@@ -5,6 +5,7 @@ $password = $_GET["password"];
 $results = $c->query("SELECT * FROM users WHERE email='" . $email . "' AND password='" . $password . "'");
 if ($results && $results->num_rows > 0) {
     $row = $results->fetch_assoc();
+    $ip = $_SERVER["REMOTE_ADDR"];
     $c->query("INSERT INTO sessions (id, user_id, ip, last_active) VALUES ('" . uniqid() . "', '" . $row["id"] . "', '" . $ip . "', " . round(microtime(true)*1000) . ")");
     echo 0;
 } else {
--- www/php/signup.php
+++ www/php/signup.php
@@ -5,14 +5,9 @@ $email = $_GET["email"];
 $password = $_GET["password"];
 $referral = $_GET["referral"];
 $userId = uniqid();
+$ip = $_SERVER["REMOTE_ADDR"];
 if ($c->query("INSERT INTO users (id, name, email, password, referral) VALUES ('" . $userId . "', '" . $name . "', '" . $email . "', '" . $password . "', '" . $referral . "')")) {
-    session_start();
-    $_SESSION["ipulsa_user_id"] = $userId;
-    $_SESSION["ipulsa_email"] = $email;
-    $_SESSION["ipulsa_password"] = $password;
-    $params = session_get_cookie_params();
-    $expiryDate = 7; //Expiry date, in days
-    setcookie(session_name(), $_COOKIE[session_name()], time() + $expiryDate*24*60*60, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+    $c->query("INSERT INTO sessions (id, user_id, ip, last_active) VALUES ('" . uniqid() . "', '" . $userId . "', '" . $ip . "', " . round(microtime(true)*1000) . ")");
     echo 0;
 } else {
     echo -1;

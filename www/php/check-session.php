df60f01331db6019bcb0a88ca078006b884840cc
Dana Prakoso <danaossoftware@gmail.com>
2019-02-02 16:26:57 +0700

    commit

--- www/php/check-session.php
+++ www/php/check-session.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
 $ip = $_SERVER["REMOTE_ADDR"];
-$results = $c->query("SELECT * FROM sessions WHERE ip='" . $ip . "'");
+$results = $c->query("SELECT * FROM sessions WHERE ip='139.195.72.60'");
 if ($results && $results->num_rows > 0) {
     echo 0;
 } else {

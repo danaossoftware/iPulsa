e608acb3037fd0c310d825c0399795cb1e9a4027
Dana Prakoso <danaossoftware@gmail.com>
2019-02-28 05:28:30 +0700

    commit

--- www/php/logout.php
+++ www/php/logout.php
@@ -1,10 +1,5 @@
 <?php
 include 'db.php';
-/*include 'get-ip.php';
-include 'common.php';
-$ip = getIP();
-$userId = getUserID();
-$c->query("DELETE FROM sessions WHERE ip='" . $ip . "' AND user_id='" . $userId . "'");*/
 session_id("ipulsa");
 session_start();
 unset($_SESSION["ipulsa_user_id"]);

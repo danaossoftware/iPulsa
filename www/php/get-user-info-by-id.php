c7c9ae2282ede450539c953a4423b2a0cbfa1adb
Dana Prakoso <danaossoftware@gmail.com>
2019-02-28 07:30:26 +0700

    commit

--- www/php/get-user-info-by-id.php
+++ www/php/get-user-info-by-id.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
 session_start();
-$userId = $_GET["user-id"];
+$userId = $_POST["user-id"];
 $results = $c->query("SELECT * FROM users WHERE id='" . $userId . "'");
 if ($results && $results->num_rows > 0) {
     echo json_encode($results->fetch_assoc());

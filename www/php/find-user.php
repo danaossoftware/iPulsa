6732da87537fad5caddb6ebe60fd47809a92eb54
Dana Prakoso <danaossoftware@gmail.com>
2019-03-04 21:44:20 +0700

    commit

--- www/php/find-user.php
+++ www/php/find-user.php
@@ -2,12 +2,10 @@
 include 'db.php';
 $key = $_POST["key"];
 $results = $c->query("SELECT * FROM users WHERE email COLLATE UTF8_GENERAL_CI LIKE '%" . $key . "%' ");
+$users = [];
 if ($results && $results->num_rows > 0) {
-    $users = [];
     while ($row = $results->fetch_assoc()) {
         array_push($users, $row);
     }
-    echo json_encode($users);
-} else {
-    echo -1;
 }
+echo json_encode($users);

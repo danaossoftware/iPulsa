5a544f985c6f62b560490523666a74816fc643e6
Dana Prakoso <danaossoftware@gmail.com>
2019-03-05 17:22:32 +0700

    commit

--- www/php/send-reset-email.php
+++ www/php/send-reset-email.php
@@ -4,8 +4,8 @@ $url = "reset-password.html?email=" . $email;
 $to      = $email;
 $subject = 'Atur ulang kata sandi BOS';
 $message = "<br/><img src='http://danaos.xyz/ipulsa/www/img/logo-2.png' width='160px' height='80px'><br/><br/><br/>Halo, " . $email . ", seseorang meminta untuk mengatur ulang kata sandi Anda. Silahkan salin URL berikut ke bilah alamat browser Anda:<br/><br/>" . $url . "<br/><br/><br/>Jika ini kesalahan pengiriman email, silahkan abaikan email ini.";
-$headers = 'From: admin@danaos.xyz' . "\r\n" .
-    'Reply-To: admin@danaos.xyz' . "\r\n" .
+$headers = 'From: admin@bospulsa.com' . "\r\n" .
+    'Reply-To: admin@bospulsa.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion() . "\r\n" .
     "Content-Type: text/html; charset=UTF-8\r\n";
 mail($to, $subject, $message, $headers);

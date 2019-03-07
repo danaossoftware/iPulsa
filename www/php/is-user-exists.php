abc8529581adbd17bed0de6cbc1d1443bddd01d3
Dana Prakoso <danaossoftware@gmail.com>
2019-03-07 23:27:12 +0700

    commit

--- www/php/is-user-exists.php
+++ www/php/is-user-exists.php
@@ -1,6 +1,6 @@
 <?php
 include 'db.php';
-$user = $_POST["$user"];
+$user = $_POST["user"];
 $results = $c->query("SELECT * FROM users WHERE email='" . $user . "' OR phone='" . $user . "'");
 if ($results && $results->num_rows > 0) {
     echo 0;

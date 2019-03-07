d74ba195ccbdfae2777a265d7035c89ed066ad45
Dana Prakoso <danaossoftware@gmail.com>
2019-03-07 23:48:00 +0700

    commit

--- www/php/update-profile.php
+++ www/php/update-profile.php
@@ -3,5 +3,4 @@ include 'db.php';
 $userId = $_POST["user_id"];
 $name = $_POST["name"];
 $phone = $_POST["phone"];
-$password = $_POST["password"];
-$c->query("UPDATE users SET name='" . $name . "', phone='" . $phone . "', password='" . $password . "' WHERE id='" . $userId . "'");
+$c->query("UPDATE users SET name='" . $name . "', phone='" . $phone . "', WHERE id='" . $userId . "'");

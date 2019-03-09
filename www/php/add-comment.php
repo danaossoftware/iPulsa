0b3b35a7abe3d292b9e1c4ec34affcff23bcea70
Dana Prakoso <danaossoftware@gmail.com>
2019-03-09 16:35:24 +0700

    commit

--- www/php/add-comment.php
+++ www/php/add-comment.php
@@ -5,5 +5,5 @@ $commendId = uniqid();
 $feedId = $_POST["post-id"];
 $comment = $_POST["comment"];
 $commenter = $_POST["user-id"];
-$c->query("INSERT INTO comments (id, feed_id, commenter, comment) VALUES ('" . $commendId . "', '" . $feedId . "', '" . $commenter . "', '" . $comment . "')");
+$c->query("INSERT INTO comments (id, feed_id, commenter, comment, date) VALUES ('" . $commendId . "', '" . $feedId . "', '" . $commenter . "', '" . $comment . "', " . round(microtime(true)*1000) . ")");
 echo $commendId;

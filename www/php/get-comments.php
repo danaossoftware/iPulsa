31d9d0e5a720a6281c557c9fe891d14c75725c5f
Dana Prakoso <danaossoftware@gmail.com>
2019-03-09 14:17:28 +0700

    commit

--- www/php/get-comments.php
+++ www/php/get-comments.php
@@ -2,7 +2,7 @@
 include 'db.php';
 $start = intval($_POST["start"]);
 $feedId = $_POST["post-id"];
-$results = $c->query("SELECT * FROM comments WHERE feed_id='" . $feedId . "'");
+$results = $c->query("SELECT * FROM comments WHERE feed_id='" . $feedId . "' ORDER BY date DESC LIMIT " . $start . ",10");
 $comments = [];
 if ($results && $results->num_rows > 0) {
     while ($row = $results->fetch_assoc()) {

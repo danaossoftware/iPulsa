6a0cb3259467a9f906210a5b0fdd42063660e5ea
Dana Prakoso <danaossoftware@gmail.com>
2019-03-09 09:36:52 +0700

    commit

--- www/php/is-post-liked.php
+++ www/php/is-post-liked.php
@@ -1,8 +1,8 @@
 <?php
 include 'db.php';
 include 'common.php';
-$feedId = $_GET["post-id"];
-$userId = getUserID();
+$feedId = $_POST["post_id"];
+$userId = $_POST["user_id"];
 $results = $c->query("SELECT * FROM likes WHERE feed_id='" . $feedId . "' AND user_id='" . $userId . "'");
 if ($results && $results->num_rows > 0) {
     echo 1;

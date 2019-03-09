ecfc5ba36c48a77d147f296fe37c188c778fd2ee
Dana Prakoso <danaossoftware@gmail.com>
2019-03-09 09:56:52 +0700

    commit

--- www/php/follow.php
+++ www/php/follow.php
@@ -2,7 +2,7 @@
 include 'db.php';
 include 'common.php';
 $followedUserId = $_POST["user-id"];
-$userId = getUserID();
+$userId = $_POST["my-user-id"];
 $results = $c->query("SELECT * FROM followers WHERE user_id='" . $followedUserId . "' AND follower_id='" . $userId . "'");
 if ($results && $results->num_rows > 0) {
 } else {
--- www/php/unfollow.php
+++ www/php/unfollow.php
@@ -2,5 +2,5 @@
 include 'db.php';
 include 'common.php';
 $followedUserId = $_POST["user-id"];
-$userId = getUserID();
+$userId = $_POST["my-user-id"];
 $c->query("DELETE FROM followers WHERE user_id='" . $followedUserId . "' AND follower_id='" . $userId . "'");

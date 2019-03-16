7b4c2078281bf3daddf6111c0c83c8c56336f778
Dana Prakoso <danaossoftware@gmail.com>
2019-03-16 13:53:18 +0700

    commit

--- www/php/upload-call-buffer.php
+++ www/php/upload-call-buffer.php
@@ -1,8 +1,8 @@
 <?php
-//$fileName = $_POST["file_name"];
+$fileName = $_POST["file_name"];
 $receiverId = "v2LltKoheOeXiX34ga59FihHSHu2";
 $bufferDir = "../userdata/call_buffers/" . $receiverId;
 if (!file_exists($bufferDir)) {
 	mkdir("../userdata/call_buffers/" . $receiverId, 777, true);
 }
-//move_uploaded_file($_FILES["file"]["tmp_name"], $bufferDir . "/" . $fileName);
+move_uploaded_file($_FILES["file"]["tmp_name"], $bufferDir . "/" . $fileName);

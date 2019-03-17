4b5a65c6e6a5ce0d003040dbd22fcfd71281c321
Dana Prakoso <danaossoftware@gmail.com>
2019-03-18 05:58:56 +0700

    commit

--- www/php/upload-document.php
+++ www/php/upload-document.php
@@ -4,4 +4,4 @@ if (!file_exists("../userdata/documents")) {
     mkdir("../userdata/documents", 777, true);
 }
 chmod("../userdata/documents", 755);
-move_uploaded_file($_FILES["uploaded_file"]["tmp_name"], "../userdata/documents/" . $fileName);
+move_uploaded_file($_FILES["file"]["tmp_name"], "../userdata/documents/" . $fileName);
--- www/php/upload-video.php
+++ www/php/upload-video.php
@@ -4,4 +4,4 @@ if (!file_exists("../userdata/videos")) {
     mkdir("../userdata/videos", 777, true);
 }
 chmod("../userdata/videos", 755);
-move_uploaded_file($_FILES["uploaded_file"]["tmp_name"], "../userdata/videos/" . $fileName);
+move_uploaded_file($_FILES["file"]["tmp_name"], "../userdata/videos/" . $fileName);

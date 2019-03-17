103dd842092e6781e5be4a64748c0f59bb1565a2
Dana Prakoso <danaossoftware@gmail.com>
2019-03-18 01:54:52 +0700

    commit

--- www/php/upload-image.php
+++ www/php/upload-image.php
@@ -1,2 +1,7 @@
 <?php
-move_uploaded_file($_FILES["file"]["tmp_name"], "../userdata/imgs/abc");
+$fileName = $_POST["file-name"];
+if (!file_exists("../userdata/imgs")) {
+    mkdir("../userdata/imgs", 777, true);
+}
+chmod("../userdata/imgs", 755);
+move_uploaded_file($_FILES["file"]["tmp_name"], "../userdata/imgs/" . $fileName);

1fd461cc5ca73862413bcee540c0c5569c40930a
Dana Prakoso <danaossoftware@gmail.com>
2019-03-18 18:31:03 +0700

    commit

--- www/php/get-messages.php
+++ www/php/get-messages.php
@@ -4,17 +4,11 @@ include 'common.php';
 $senderId = $_POST["sender-id"];
 $receiverId = $_POST["receiver-id"];
 $start = intval($_POST["start"]);
-$results = $c->query("SELECT * FROM messages WHERE sender_id='" . $senderId . "' AND receiver_id='" . $receiverId . "' ORDER BY sent_date DESC LIMIT " . $start . ",10");
+$results = $c->query("SELECT * FROM messages WHERE (sender_id='" . $senderId . "' AND receiver_id='" . $receiverId . "') OR (sender_id='" . $receiverId . "' AND receiver_id='" . $senderId . "') ORDER BY sent_date DESC LIMIT " . $start . ",10");
 $messages = [];
 if ($results && $results->num_rows > 0) {
     while ($row = $results->fetch_assoc()) {
         array_push($messages, $row);
     }
-}
-$results = $c->query("SELECT * FROM messages WHERE sender_id='" . $receiverId . "' AND receiver_id='" . $senderId . "' ORDER BY sent_date DESC LIMIT " . $start . ",10");
-if ($results && $results->num_rows > 0) {
-	while ($row = $results->fetch_assoc()) {
-		array_push($messages, $row);
-	}
 }
 echo json_encode($messages);

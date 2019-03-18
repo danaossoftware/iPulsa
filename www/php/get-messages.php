7ba2a3717088ecb9fbb5af0ed0fedf1297141d07
Dana Prakoso <danaossoftware@gmail.com>
2019-03-18 18:29:16 +0700

    commit

--- www/php/get-messages.php
+++ www/php/get-messages.php
@@ -5,8 +5,8 @@ $senderId = $_POST["sender-id"];
 $receiverId = $_POST["receiver-id"];
 $start = intval($_POST["start"]);
 $results = $c->query("SELECT * FROM messages WHERE sender_id='" . $senderId . "' AND receiver_id='" . $receiverId . "' ORDER BY sent_date DESC LIMIT " . $start . ",10");
+$messages = [];
 if ($results && $results->num_rows > 0) {
-    $messages = [];
     while ($row = $results->fetch_assoc()) {
         array_push($messages, $row);
     }

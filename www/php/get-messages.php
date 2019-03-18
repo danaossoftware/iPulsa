1bf7db591fbe5c9e2670f443a7f393800fad51e2
Dana Prakoso <danaossoftware@gmail.com>
2019-03-18 18:21:17 +0700

    commit

--- www/php/get-messages.php
+++ www/php/get-messages.php
@@ -10,14 +10,11 @@ if ($results && $results->num_rows > 0) {
     while ($row = $results->fetch_assoc()) {
         array_push($messages, $row);
     }
-    echo json_encode($messages);
-} else {
-    $results = $c->query("SELECT * FROM messages WHERE sender_id='" . $receiverId . "' AND receiver_id='" . $senderId . "' ORDER BY sent_date DESC LIMIT " . $start . ",10");
-	$messages = [];
-    if ($results && $results->num_rows > 0) {
-        while ($row = $results->fetch_assoc()) {
-            array_push($messages, $row);
-        }
-    }
-	echo json_encode($messages);
 }
+$results = $c->query("SELECT * FROM messages WHERE sender_id='" . $receiverId . "' AND receiver_id='" . $senderId . "' ORDER BY sent_date DESC LIMIT " . $start . ",10");
+if ($results && $results->num_rows > 0) {
+	while ($row = $results->fetch_assoc()) {
+		array_push($messages, $row);
+	}
+}
+echo json_encode($messages);

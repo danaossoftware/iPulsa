db5c6da160d5d0e621bfe44fc3c54bb68deb9747
Dana Prakoso <danaossoftware@gmail.com>
2019-03-18 15:43:45 +0700

    commit

--- www/php/reply-message.php
+++ www/php/reply-message.php
@@ -5,5 +5,6 @@ $senderId = $_POST["sender-id"];
 $receiverId = $_POST["receiver-id"];
 $message = $_POST["message"];
 $messageId = uniqid();
-$c->query("INSERT INTO messages (id, sender_id, receiver_id, message, reply_to_id) VALUES ('" . $messageId . "', '" . $senderId . "', '" . $receiverId . "', '" . $message . "', '" . $messageId . "')");
+$sentDate = round(microtime(true)*1000);
+$c->query("INSERT INTO messages (id, sender_id, receiver_id, message, reply_to_id, sent_date) VALUES ('" . $messageId . "', '" . $senderId . "', '" . $receiverId . "', '" . $message . "', '" . $messageId . "', " . $sentDate . ")");
 echo json_encode($c->query("SELECT * FROM messages WHERE id='" . $messageId . "'")->fetch_assoc());

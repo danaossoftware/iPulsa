c421df465bf291c0d08cee44ae04adc020f89706
Dana Prakoso <danaossoftware@gmail.com>
2019-03-19 00:38:50 +0700

    commit

--- www/php/assign-access-token.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'db.php';
-$userId = $_POST["user-id"];
-$token = $_POST["token"];
-$c->query("UPDATE users SET fcm_access_token='" . $token . "' WHERE id='" . $userId . "'");
--- www/php/get-access-token.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'db.php';
-$userId = $_POST["user-id"];
-$results = $c->query("SELECT * FROM users WHERE id='" . $userId . "'");
-$row = $results->fetch_assoc();
-$accessToken = $row["fcm_access_token"];
-echo $accessToken;
